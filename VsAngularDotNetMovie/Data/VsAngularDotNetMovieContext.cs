f5dc7fc2bfdf2b063967e2e53b6ae3ea22605e69
screamatthewind <4452527+screamatthewind@users.noreply.github.com>
2018-05-08 07:54:45 -0400

    Initial Commit

---
+++ VsAngularDotNetMovie/Data/VsAngularDotNetMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VsAngularDotNetMovie;
+
+namespace VsAngularDotNetMovie.Models
+{
+    public class VsAngularDotNetMovieContext : DbContext
+    {
+        public VsAngularDotNetMovieContext (DbContextOptions<VsAngularDotNetMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VsAngularDotNetMovie.Movie> Movie { get; set; }
+    }
+}
---
+++ VsAngularDotNetMovie/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using VsAngularDotNetMovie.Models;
+
+namespace VsAngularDotNetMovie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<VsAngularDotNetMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("VsAngularDotNetMovieContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
